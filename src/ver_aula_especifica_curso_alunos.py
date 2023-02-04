64dd63eb8c821e6ea7cd89f59cd90957dabc49bc
arthur-augusto <arthur.augusto.rp@gmail.com>
2023-02-04 17:35:39 -0300

    adicionando ver_aula_especifica_curso_alunos.py

---
+++ src/ver_aula_especifica_curso_alunos.py
@@ -0,0 +1,10 @@
+from geral import *
+from callback_com_dados import *
+
+class VerAulaEspecificaCursoAlunos(CallbackComDados):
+    async def lida_callback(update: Update, context: ContextTypes.DEFAULT_TYPE, dados):
+
+
+        #TODO
+
+        return await super().lida_callback(context, dados)
