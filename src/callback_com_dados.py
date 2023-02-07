38f4491a2b9163ec3100b2947c2bda96310c995e
arthur-augusto <arthur.augusto.rp@gmail.com>
2023-02-06 22:50:58 -0300

    refatorando callback_com_dados.py

--- src/callback_com_dados.py
+++ src/callback_com_dados.py
@@ -12,21 +12,35 @@ from copy import deepcopy
 import pandas as pd
 import time
 
-from geral import call_database_and_execute, hash_string
+from geral import call_database_and_execute, hash_string,get_all_subclasses,add_set_update_and_context_to_function_call
 from callback import Callback
 
+async def handle_generic_callback(update: Update,context: ContextTypes.DEFAULT_TYPE):
+
+    query = update.callback_query.data
+
+    if len(query.split(' ')) > 1:
+        descricao_ordem, dados = query.split(' ')
+        if dados == ";":
+            dados = ""
+        if len(dados.split(',')) > 1:
+            dados = dados.split(',')
+        for subclass in get_all_subclasses(CallbackComDados):
+            if descricao_ordem == subclass.__name__:
+                await add_set_update_and_context_to_function_call(subclass.lida_callback)(update,context,dados)
 
 class CallbackComDados(Callback):
-    async def lida_callback(update: Update, context: ContextTypes.DEFAULT_TYPE,dados):
+    async def lida_callback(update: Update, context: ContextTypes.DEFAULT_TYPE,*dados):
         pass
-        return await Callback.lida_callback(update,context)
+        return await super().lida_callback(update,context)
 
-    def __init__(self,dados):
+    def __init__(self,*dados):
         self._dados = dados
-
-    def get_dados(self):
-        return self._dados
-
     
     def get_callback_str(self):
-        return f'{self.__class__.__name__} {self.get_dados()}'
+        return f'{self.__class__.__name__} {",".join(list(map(lambda x: str(x),self._dados)))}'
+    
+    @staticmethod
+    def add_to_application(application):
+        application.add_handler(CallbackQueryHandler(callback=handle_generic_callback))
+
