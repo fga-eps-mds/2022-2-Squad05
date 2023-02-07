32aefe5bfc0ec7fe836d46fcc3e3588c5147d4c9
arthur-augusto <arthur.augusto.rp@gmail.com>
2023-02-06 22:52:20 -0300

    refatorando callback_sem_dados.py

--- src/callback_sem_dados.py
+++ src/callback_sem_dados.py
@@ -12,13 +12,20 @@ from copy import deepcopy
 import pandas as pd
 import time
 
-from geral import call_database_and_execute, hash_string
+from geral import call_database_and_execute, hash_string,get_all_subclasses,add_set_update_and_context_to_function_call
 from callback import Callback
 
-
+import os
+import inspect
 
 class CallbackSemDados(Callback):
 
     
     def get_callback_str(self):
         return f'{self.__class__.__name__}'
+    
+    @staticmethod
+    def add_to_application(application):
+        for subclasse in get_all_subclasses(CallbackSemDados):
+            application.add_handler(CallbackQueryHandler(callback=add_set_update_and_context_to_function_call(subclasse.lida_callback),pattern=subclasse.__name__))
+    
