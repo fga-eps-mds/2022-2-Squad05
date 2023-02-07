cdf72f610a3e43efd0a8d88279826ca59288dc00
Ana Letícia <56135971+analeticiaa@users.noreply.github.com>
2023-02-06 22:06:21 -0300

    Update test_telegram.py

--- tests/test_telegram.py
+++ tests/test_telegram.py
@@ -1,13 +1,39 @@
 from geral import *
 from all import inicia_bot
+from bot_cursos import start as bot_cursos_start
 import pytest
 import asyncio as a
+from cursos import *
+import time
+from datetime import datetime
+
+import pytest
+
+from telegram import (
+    CallbackQuery,
+    Chat,
+    ChatJoinRequest,
+    ChatMemberOwner,
+    ChatMemberUpdated,
+    ChosenInlineResult,
+    InlineQuery,
+    Message,
+    Poll,
+    PollAnswer,
+    PollOption,
+    PreCheckoutQuery,
+    ShippingQuery,
+    Update,
+    User,
+)
 
 BOTE_TESTS_TOKEN = "5666314930:AAENO8ook7pmT3u9dP_x8wPE03_wWMhdnFM"
 
 BOTEZINHO_TESTS_TOKEN = "5768246642:AAGNaoBYQcR7O0p7aNCyA_tBX0sUw5ps2pE"
 
 TESTES_CHAT_ID = -605298131
+
+from telegram._utils.datetime import from_timestamp
 
 #todos os testes desse arquivo devem primeiro chamar inicia bot
 def test_cria_processo():
@@ -18,19 +44,3 @@ def test_cria_processo():
 
     processo_alunos.terminate()
     processo_cursos.terminate()
-
-@pytest.mark.asyncio
-async def test_manda_start_bote():
-    processo_cursos,processo_alunos = inicia_bot(BOTEZINHO_TESTS_TOKEN,BOTE_TESTS_TOKEN)
-    
-    
-    botezinho = telegram.Bot(BOTEZINHO_TESTS_TOKEN)
-    bote = telegram.Bot(BOTE_TESTS_TOKEN)
-    await botezinho.initialize()
-    await bote.initialize()
-    message = await botezinho.send_message(TESTES_CHAT_ID,"@bote_tests_bot /start")
-
-    chat = await bote.get_chat(message.chat_id)
-
-    await a.sleep(5)
-
